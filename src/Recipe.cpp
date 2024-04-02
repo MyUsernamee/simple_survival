0c12f51c03ba57ab7e1ac4015ac233d66f5c46f8
MyUsernamee <delanoleslieprof@gmail.com>
2024-04-02 15:49:24 -0700

    Add = sign to Recipe and fix rendering, remove Render Texture

--- src/Recipe.cpp
+++ src/Recipe.cpp
@@ -8,23 +8,20 @@ void Recipe::render(int x, int y, int width, int height)
     DrawRectangle(x, y, width, height, UI_BG_COLOR);
     DrawRectangleLines(x, y, width, height, UI_BORDER_COLOR);
 
-    raylib::RenderTexture render_texture(width, height);
-
-    render_texture.BeginMode();
-
     // From the left 
     for(int i = 0; i < ingredients.size(); i++)
     {
         Slot slot = ingredients[i];
-        slot.render(i * 16 + 8, height / 2 - 8, 16, 16);
+        slot.render(i * 16 + 8 + x, height / 2 - 8 + y, 16, 16);
     }
 
     // From the right
-    result.render(width - 16 - 8, height / 2 - 8, 16, 16);
+    result.render(width - 16 - 8 + x, height / 2 - 8 + y, 16, 16);
 
-    render_texture.EndMode();
+    raylib::Text text = raylib::Text("="); // TODO: Add a fun font to the game
+    text.SetColor(WHITE);
+    text.Draw(raylib::Vector2(width - 28 + x - text.MeasureEx().x / 2.0, height / 2 + y - text.MeasureEx().y / 2.0));
 
-    DrawTexture(render_texture.getTexture(), x, y, WHITE);
 }
 
 Recipe::Recipe(std::vector<Slot> ingredients, Slot result)
